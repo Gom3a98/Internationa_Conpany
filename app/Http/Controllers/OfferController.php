d6e46abd03b67cf23ea5911a3bf1f212bf459b55
amrfahmy5 <elkingamr46@yahoo.com>
2020-06-26 05:28:39 +0200

    ---

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -85,7 +85,7 @@ class OfferController extends Controller
         $sales_array =$request->sales;
         $sales = array();
         foreach($sales_array as $sale){
-            $query ='insert into offer_product (productCount, productPrice, product_id, offer_id,created_at,updated_at)
+            $query ='insert into offer_product ("productCount", "productPrice", "product_id", "offer_id",created_at,updated_at)
              values ('.$sale["product_count"]. ',' .$sale["price"]. ','. $sale["product_id"]. ','.
               $offer->id .',0,0)';
               try { 
