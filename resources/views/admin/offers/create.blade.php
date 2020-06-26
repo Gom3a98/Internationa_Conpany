c8e62ba2b58c9b8ec037a4f38046e1f18d02844c
amrfahmy5 <elkingamr46@yahoo.com>
2020-06-26 02:11:52 +0200

    fuck

--- resources/views/admin/offers/create.blade.php
+++ resources/views/admin/offers/create.blade.php
@@ -1,11 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
 <script
   src="https://code.jquery.com/jquery-3.5.1.js"
   integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
   crossorigin="anonymous"></script>
-@extends('admin.layouts.admin')
-
-@section('Contents')
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
 <script>
         var Selectedid=-1;
@@ -90,6 +96,11 @@
         return inputValues;
     }
 </script>
+
+
+<body>
+    
+
 <div style="text-align: right" class="conrainer" dir="rtl">
     <div class="box box-warning">
         <div class="box-header with-border">
@@ -157,4 +168,5 @@
     </div>
 </div>
 
-@endsection
+</body>
+</html>
