222b19b0859e2836eeb0bba051d9a568b671a882
amrfahmy5 <elkingamr46@yahoo.com>
2020-06-26 05:54:17 +0200

    forgetPart

--- resources/views/admin/offers/create.blade.php
+++ resources/views/admin/offers/create.blade.php
@@ -62,8 +62,8 @@
                     data: toSend,
                     success: function(result) {
                         console.log(result)
-                        // var url = '/admin/offers';
-                        // var myWindow = window.open(url, "_self", "width=1200, height=600,scrollbars=yes,status=yes,location = yes");
+                        var url = '/admin/offers';
+                        var myWindow = window.open(url, "_self", "width=1200, height=600,scrollbars=yes,status=yes,location = yes");
                         }
                     });
                 // }
