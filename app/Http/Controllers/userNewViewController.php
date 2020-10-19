09e1031a709935fb3238bd689eb1df7c94763662
amrfahmy5 <elkingamr46@yahoo.com>
2020-10-19 14:20:09 +0200

    company details

--- app/Http/Controllers/userNewViewController.php
+++ app/Http/Controllers/userNewViewController.php
@@ -171,8 +171,8 @@ class userNewViewController extends Controller
     public function search(Request $request)
     {
         $key= $request->get('key');
-        $products = $this->product->select('products.*','product_images.url')->join('product_images','product_images.product_id','=','products.id')->where('product_images.main','1')->where('products.name', 'regexp',$key)->get();
-        
+        // $products = $this->product->select('products.*','product_images.url')->join('product_images','product_images.product_id','=','products.id')->where('product_images.main','1')->where('products.name', 'regexp',$key)->get();
+        $products = $this->product->select('products.*','product_images.url')->join('product_images','product_images.product_id','=','products.id')->where('product_images.main','1')->get();
         $categories = $this->getCategories();
         $categoriesProduct=Category::with(array('products'=>function($query){
             $query->select('products.*','product_images.url')->join('product_images','product_images.product_id','=','products.id')->where('product_images.main','1')->get();
